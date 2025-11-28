79b4e026958ae824758b2a4ca122ffea5cf251e3
krzysztof <u173_dolkrz_waw@technischools.com>
2025-11-28 23:20:35 +0100

    fix workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_security_group" "banking_web" {
-  name        = "banking-web-sg-v10"
-  description = "Allow HTTP, HTTPS, SSH"
+resource "aws_security_group" "banking_sg" {
+  name        = "banking-sg"
+  description = "Banking app security group"
 
   ingress {
     from_port   = 80
@@ -27,21 +27,34 @@ resource "aws_security_group" "banking_web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "banking-sg"
+  }
 }
 
 resource "aws_instance" "banking_server" {
+  count         = 2
+
   ami           = "ami-0084a47cc718c111a"
   instance_type = "t3.small"
   key_name      = "bank"
-  
-  vpc_security_group_ids = [aws_security_group.banking_web.id]
-  
+
+  vpc_security_group_ids = [aws_security_group.banking_sg.id]
+
   user_data = <<-EOF
               #!/bin/bash
               apt-get update
@@ -49,13 +62,25 @@ resource "aws_instance" "banking_server" {
               systemctl start docker
               systemctl enable docker
               usermod -aG docker ubuntu
+
+              mkdir -p /home/ubuntu/banking
+              chown ubuntu:ubuntu /home/ubuntu/banking
               EOF
-  
+
   tags = {
-    Name = "Banking-Server"
+    Name = "Banking-Server-${count.index + 1}"
   }
 }
 
-output "server_ip" {
-  value = aws_instance.banking_server.public_ip
+# Output IPs of both servers
+output "server_ips" {
+  value = aws_instance.banking_server[*].public_ip
+}
+
+output "server_1_ip" {
+  value = aws_instance.banking_server[0].public_ip
+}
+
+output "server_2_ip" {
+  value = aws_instance.banking_server[1].public_ip
 }
