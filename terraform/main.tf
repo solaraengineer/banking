00f8d380023b9ab0c0be4025be18123ba0575241
krzysztof <u173_dolkrz_waw@technischools.com>
2025-11-28 11:03:27 +0100

    MVP

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_security_group" "banking_web" {
-  name        = "banking-web-sg"
+  name        = "banking-web-sg-v2"
   description = "Allow HTTP, HTTPS, SSH"
 
   ingress {
